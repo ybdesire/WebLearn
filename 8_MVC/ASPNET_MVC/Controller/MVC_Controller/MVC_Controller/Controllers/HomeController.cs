7e5117ca1ac521b638db6827196328245235a745
ybdesire <ybdesire@gmail.com>
2015-01-07 04:54:19 -0500

    Add a controller only demo for 8_MVC/ASPNET_MVC.

---
+++ 8_MVC/ASPNET_MVC/Controller/MVC_Controller/MVC_Controller/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Controller.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Home is the controller name
+        // GET: /Home/
+
+        public string Index()
+        {
+            return "this is the default access by 'url/' or 'url/Home' or 'url/Home/Index'";
+        }
+
+        public string Action1()
+        {
+            return "Please access this function by url/Home/Action1";
+        }
+
+        public string Action2(string name)
+        {
+            string intro = "Pleas access this function by url/Home/Action2?name=xxx;            ";
+            string nameInfo = "This is " + name;
+            return intro + nameInfo;
+        }
+
+        //Pleas access this function by url/Home/Action3/123
+        public string Action3(string ID)//if the function parameter is id/Id/ID, the id at RouteConfig.cs will come here
+        {
+            return "id is " + ID;
+        }
+    }
+}
---
+++ 8_MVC/ASPNET_MVC/Controller/readMe.txt
@@ -0,0 +1,9 @@
+MVC_Controller is a MVC4 controller project
+
+Pease check more details at the 2 files below.
+
+WebLearn\8_MVC\ASPNET_MVC\Controller\MVC_Controller\MVC_Controller\App_Start\RouteConfig.cs
+	this file defined the router. (url parse rule)
+
+WebLearn\8_MVC\ASPNET_MVC\Controller\MVC_Controller\MVC_Controller\Controllers\HomeController.cs
+	this is the router source code.
