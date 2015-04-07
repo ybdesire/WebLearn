54f58de788e8ac64c3d2b88b433b836b93c0912a
ybdesire <ybdesire@gmail.com>
2015-04-07 03:17:59 -0400

    Add a username and password input process frame.

---
+++ 8_MVC/ASPNET_MVC/Session_MVC_View/MVC_View/MVC_View/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_View.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index() //Access this by "url" or "url/Home/"
+        {
+            return View();
+        }
+
+        public ActionResult Hello() //Access this by "url/Home/Hello"
+        {
+            ViewBag.Message = "Hello view";//pass value from controller to view
+            return View();
+        }
+
+        //The 'event' function for button "Sign In" at Index.cshtml
+        public String SignIn(string userName, string passwd)
+        {
+            return userName + "   " + passwd;
+        }
+
+    }
+}
