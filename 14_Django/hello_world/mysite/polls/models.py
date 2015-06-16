c2e7e537282bdae5c4408e88495ce3e716c70c07
ybdesire <ybdesire@gmail.com>
2015-06-16 22:06:54 +0800

    First Django app from https://docs.djangoproject.com/en/1.8/intro/tutorial01/

--- 14_Django/hello_world/mysite/polls/models.py
+++ 14_Django/hello_world/mysite/polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+
+class Question(models.Model):
+	def __str__(self):
+		return self.question_text
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	
+class Choice(models.Model):
+	def __str__(self):
+		return self.question_text
+	question = models.ForeignKey(Question)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
