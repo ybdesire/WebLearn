7468b216b5bd13aa8d9cd3297a0f1f4a403f955a
ybdesire <ybdesire@gmail.com>
2015-06-22 20:07:45 +0800

    Completed 'Writing more views' at https://docs.djangoproject.com/en/1.8/intro/tutorial03/

--- 14_Django/Writing_your_first_Django_app/mysite/polls/urls.py
+++ 14_Django/Writing_your_first_Django_app/mysite/polls/urls.py
@@ -3,5 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	#/polls/
 	url(r'^$', views.index, name='index'),
+	#/polls/5/
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	#/polls/5/results/
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	#/polls/5/vote/
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+	
 ]
