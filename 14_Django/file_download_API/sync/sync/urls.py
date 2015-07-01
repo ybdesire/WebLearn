a7f63b9b89c76c4027fc0a6c0c8942e7f1d44c21
ybdesire <ybdesire@gmail.com>
2015-07-01 09:56:31 -0400

    Init 'upload' url & view.

--- 14_Django/file_download_API/sync/sync/urls.py
+++ 14_Django/file_download_API/sync/sync/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^download/', include('download.urls')),
+	url(r'^upload/', include('upload.urls')),
 
 ]
--- 14_Django/file_download_API/sync/upload/views.py
+++ 14_Django/file_download_API/sync/upload/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import subprocess
+import os.path
 
 # Create your views here.
+def index(request):
+	return HttpResponse('this is upload view!')
