3c2a95ebec396bff5920b6536ce98f4e69bc97d0
ybdesire <ybdesire@gmail.com>
2015-07-08 03:31:35 -0400

    Add Django template i18n demo.

--- 14_Django/I18nDjango/i18n/views.py
+++ 14_Django/I18nDjango/i18n/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.translation import ugettext
+from django.template import RequestContext, loader
+import os
+
 # Create your views here.
 
 
 def index(request):
 	return HttpResponse(ugettext('hello world!'))
+	
+
+def page(request):
+	template = loader.get_template('tmp.html')
+	context = RequestContext(request)
+	return HttpResponse(template.render(context))
