210c3eb060c7f5168b19f2f870bea115e2f2301b
ybdesire <ybdesire@gmail.com>
2016-04-12 14:58:45 +0800

    Parse sub-category by scrapy.

---
+++ 23_scrapy/3_sub_category/hello/hello/spiders/hello_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+from hello.items import HelloItem
+
+class HelloSpider(scrapy.Spider):
+    name = "hello"#identifies the Spider. It must be unique
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+    "http://www.dmoz.org/Computers/Programming/Languages/Python/",
+    ]
+
+    def parse(self, response):
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            print '[url] '+url
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = HelloItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
---
+++ 23_scrapy/3_sub_category/readme.md
@@ -0,0 +1,25 @@
+Parse sub-category
+===================
+
+How to parse the hrefs (sub-category) at the page below.
+http://www.dmoz.org/Computers/Programming/Languages/Python/
+
+
+## Environment 
+
+* OS: Windows 10
+* Python: python 2.7
+* Python 3rd libs
+   * scrapy
+   * virtualenv
+   * pywin32:¡¡pip install pypiwin32
+
+
+## Steps
+### 1. copy project 'hello'
+
+
+### 2. add code
+http://doc.scrapy.org/en/latest/intro/tutorial.html#following-links
+
+### 3. Run spider and get result
