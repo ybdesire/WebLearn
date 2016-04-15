33ecd89122c1aa7d93b94e87277b8b5ce176972d
ybdesire <ybdesire@gmail.com>
2016-04-15 17:27:31 +0800

    Download a file by URL - python.

---
+++ 15_python/network/py_download_file_by_url.py
@@ -0,0 +1,10 @@
+import urllib.request
+url3 = 'https://www.apkfiles.com/download/173438/network-manager-game-strategy-2015.apk?key=3b1adb1aa56f71c524ea71d14a8c898f HTTP/1.1'
+file_name3 = 'apk3'
+
+url4 = 'https://www.apkfiles.com/download/136038/calculator-1-4-3-app-wallpaper.apk?key=4da041e729bbfe7fee461e68ec119f5d'
+file_name4 = 'apk4'
+
+urllib.request.urlretrieve(url4, file_name4)
+
+
